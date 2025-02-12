953f2f25748fc6529efddc43674a4df771f1c826
mateushr23 <mateushr23@gmail.com>
2025-02-12 14:10:36 -0300

    feat: ajust deploy settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"], // Separa pacotes de vendor
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500, // Opcional: Aumenta o limite do aviso
+  },
+});
