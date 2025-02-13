868d6311e8938f3cdfcb187e111920ed7970c41a
mateushr23 <mateushr23@gmail.com>
2025-02-13 20:36:54 -0300

    Add console log

--- src/pages/callback/index.tsx
+++ src/pages/callback/index.tsx
@@ -1,16 +1,24 @@
 import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 export function AuthCallback() {
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  console.log("AuthCallback montado"); // Verifique se isto aparece no console
 
   useEffect(() => {
     const token = searchParams.get("token");
+    console.log("Token recebido:", token); // Isso também deve aparecer
+
     if (token) {
       localStorage.setItem("jwt", token);
-      window.location.href = "/home"; // Redireciona para home ou outra página
+      navigate("/home"); // Redireciona para home ou outra página
+    } else {
+      console.error("Token não encontrado na URL.");
+      navigate("/"); // Redireciona para a página de login
     }
-  }, [searchParams]);
+  }, [searchParams, navigate]);
 
   return <p>Autenticando...</p>;
-};
+}
