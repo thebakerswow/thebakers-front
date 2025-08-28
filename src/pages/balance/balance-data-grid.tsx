d8f753469b494ec04325bc1a908b2237da236545
mateushr23 <mateushr23@gmail.com>
2025-08-27 21:00:24 -0300

    refactor: remove unused useCallback import in balance-data-grid component for cleaner code

--- src/pages/balance/balance-data-grid.tsx
+++ src/pages/balance/balance-data-grid.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo, useCallback } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 
 import axios from 'axios'
 import { format, eachDayOfInterval, parseISO } from 'date-fns'
